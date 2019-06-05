49fba410b223acf8996b890e1beacb478d958557
oovg <office@vengist.com>
2019-06-05 13:44:57 -0700

    removes typeform for testing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import { ReactTypeformEmbed } from 'react-typeform-embed'
 
 import Layout from '../components/layout/layout'
 
@@ -9,13 +8,6 @@ import '../styles/global.scss';
 
 
 class IndexPage extends React.Component {
-  state = {
-    open: false
-  }
-
-  openForm = () => {
-    this.typeformEmbed.typeform.open();
-  }
 
   render () {
     return (
@@ -33,13 +25,6 @@ class IndexPage extends React.Component {
           <h2>Block #2</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           <button className="button" onClick={this.openForm}>Join Us</button>
-            <ReactTypeformEmbed
-              url={'https://ven386248.typeform.com/to/rEhanV'}
-              popup={true}
-              autoOpen={false}
-              style={{height: '0%'}}
-              ref={(tf => this.typeformEmbed = tf) }
-            />
         </div>
       </div>
       <div className="Block">

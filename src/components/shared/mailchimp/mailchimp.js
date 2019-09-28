950aad58056070758418cc3fd0b939a2525d7acd
Francis Storr <fstorr@protonmail.com>
2019-09-28 08:49:08 -0700

    Improve accessibility of form elements
    - Added `<label>` elements, remove `placeholder` attributes. (WCAG 4.1.2 Name, Role, Value and others)
    - Added `autocomplete` attributes (WCAG 1.3.5 Identify Input Purpose)
    - Changed `type=text` to `type=email` for email input (WCAG 4.1.2)
    - Added `aria-required=“true”` to email as it seems to be the only field that’s required.
    - Added visible, but hidden to assistive technology, “required” text to the email input.
    - Connected “Result” section to email input with `aria-describedby` so that error messages are announced to screen readers.
    - ` Changed “Result” element from a `<p>` to an `<output>` which has better semantics and announces the error to screen readers.
    - Added `formnovalidate` to the Submit button in case browsers try to validate the new `email` input.

--- src/components/shared/mailchimp/mailchimp.js
+++ src/components/shared/mailchimp/mailchimp.js
@@ -7,37 +7,73 @@ class Mailchimp extends React.Component {
     error: false,
   }
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    const formData = new FormData(this.form);
+  handleSubmit = e => {
+    e.preventDefault()
+    const formData = new FormData(this.form)
     const email = formData.get('EMAIL')
     const fields = {
-        FNAME: formData.get('FNAME'),
-        LNAME: formData.get('LNAME'),
+      FNAME: formData.get('FNAME'),
+      LNAME: formData.get('LNAME'),
     }
-    console.log(email);
-    console.log(fields);
-    addToMailchimp(email, fields).then((result) => {
+    console.log(email)
+    console.log(fields)
+    addToMailchimp(email, fields).then(result => {
       this.setState({
         resultMessage: result.msg.split('<a')[0],
         error: result.result === 'error',
       })
-      this.form.reset();
-    });
+      this.form.reset()
+    })
   }
- 
-  render () {
-    const resultMessage = this.state.error ? "error-message" : "success-message";
+
+  render() {
+    const resultMessage = this.state.error ? 'error-message' : 'success-message'
 
     return (
-      <form onSubmit={this.handleSubmit} name="emailForm" ref={(el) => this.form = el} autoComplete="off">
-        <input type="text" name="FNAME" placeholder="First Name" />
-        <input type="text" name="LNAME" placeholder="Last Name" />
-        <input type="text" name="EMAIL" placeholder="Email Address" />
-        <button type="submit">Sign up</button>
-        <div className="Result">
-          <p className={resultMessage}>{this.state.resultMessage}</p>
+      <form
+        onSubmit={this.handleSubmit}
+        name="emailForm"
+        ref={el => (this.form = el)}
+      >
+        <div className="formGroup">
+          <input
+            autocomplete="given-name"
+            id="fname"
+            name="FNAME"
+            type="text"
+          />
+          <label for="fname">First Name</label>
         </div>
+        <div className="formGroup">
+          <input
+            autocomplete="family-name"
+            id="lname"
+            name="LNAME"
+            type="text"
+          />
+          <label for="lname">Last Name</label>
+        </div>
+        <div className="formGroup">
+          <input
+            aria-describedby="email-error"
+            aria-required="true"
+            autocomplete="email"
+            id="email"
+            name="EMAIL"
+            type="email"
+          />
+          <label for="email">
+            Email Address<span aria-hidden="true"> (required)</span>
+          </label>
+          <div className="Result">
+            <output className={resultMessage} id="email-error">
+              {this.state.resultMessage}
+            </output>
+          </div>
+        </div>
+        <button formnovalidate="true" type="submit">
+          Sign up
+        </button>
       </form>
     )
   }
